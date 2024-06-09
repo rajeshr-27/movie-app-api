61a29138dc7b63015c50b533cea47919f6e70c7a
rajesh-27 <rajeshkphp27@gmail.com>
2024-06-09 09:14:30 +0530

    updated code

--- controller/fileUploadController.js
+++ controller/fileUploadController.js
@@ -6,14 +6,16 @@ const asyncHandler = require('express-async-handler')
 const uploadFile = asyncHandler(async(req,res)=> {
     const https = require('https');
     const fs = require('fs');
-    //const  url = 'https://download.samplelib.com/mp4/sample-5s.mp4';
-   // const localPath = __basedir+"/uploads/videos";
-  // const localPath ="./uploads/videos/sample_file.mp4";
+
    const {url,file_name} = req.query
    if(url && file_name){
        
         const localPath =`./uploads/videos/${file_name}`;
+
         https.get(url, (response) => { 
+            if (response.statusCode !== 200) {
+                return res.status(response.statusCode).send('Failed to download file');
+            }
             const file = fs.createWriteStream(localPath);
             response.pipe(file);
             file.on('finish', () => {
@@ -22,10 +24,16 @@ const uploadFile = asyncHandler(async(req,res)=> {
                     res.send('upload success');
                 })
             })
+            file.on('error', (err) => {
+                fs.unlink(localPath, () => { // Clean up the partially downloaded file
+                    console.error('Error writing file', err);
+                    res.status(500).send('Error writing file');
+                });
+            });
         }).on('error',(err) => {
             console.log(err);
         })
-        res.send('upload success');
+        
    }
 
 
